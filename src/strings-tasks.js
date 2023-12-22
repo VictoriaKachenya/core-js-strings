aee26d1675a06f91b13b96d736573ac68dfc8f51
Victoria <victoriakachenya@gmail.com>
2023-12-22 14:53:06 +0300

    feat: added some solutions in strings tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,14 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const index = str.indexOf(value);
+  if (index !== -1) {
+    const arrStr = str.split('');
+    arrStr.splice(index, value.length);
+    return arrStr.join('');
+  }
+  return str;
 }
 
 /**
@@ -162,8 +168,14 @@ function removeFirstOccurrences(/* str, value */) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const index = str.lastIndexOf(value);
+  if (index !== -1) {
+    const arrStr = str.split('');
+    arrStr.splice(index, value.length);
+    return arrStr.join('');
+  }
+  return str;
 }
 
 /**
